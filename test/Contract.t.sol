9060e1d2841c064ec97d27257b4185f2e2663507
Philippe Dumonet <philippe@dumo.net>
2022-07-31 20:56:35 +0200

    WIP

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 
 contract ContractTest is Test {
     function setUp() public {}
